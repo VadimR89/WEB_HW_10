ae11449ee3c351108f19cc75aa384145e4af0103
VadimR89 <136196738+VadimR89@users.noreply.github.com>
2024-04-14 12:53:36 +0300

    Add files via upload

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Author(models.Model):
+    fullname = models.CharField(max_length=100, null=False, unique=True)
+    born_date = models.CharField(max_length=50, null=False)
+    born_location = models.CharField(null=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.fullname}"
+
+
+class PasswordResetToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.UUIDField(default=uuid.uuid4, editable=False)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 
 def login_page(request):
@@ -19,3 +23,12 @@ def login_page(request):
 def logout_page(request):
     logout(request)
     return redirect('login')
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
